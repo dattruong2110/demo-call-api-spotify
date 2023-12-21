32f0d6132fe959cea7a13c6e1f6eb98d89d81579
Trương Tấn Đạt <tandattruong2110@gmail.com>
2023-12-21 14:41:37 +0700

    first commit

---
+++ src/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+      ClientId: "c1eab79c58824381b38eadd1d3aff129",
+      ClientSecret: "0b4433ceb56246c1894f466829f4bfd1",
+    };
+}
+
+export { Credentials };
